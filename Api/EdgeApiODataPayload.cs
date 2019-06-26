63233d8ca7d07922d9dfedb1602499f9c0e388db
Roger Woodley <rwoodley@csod.com>
2019-06-26 07:05:56 -0500

    EAPI-2381 | Move Existing Solution From TFS

---
+++ Api/EdgeApiODataPayload.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Api
+{
+    [DataContract]
+    public class EdgeApiODataPayload
+    {
+        [DataMember(Name = "@odata.context")]
+        public string Context { get; set; }
+
+        [DataMember(Name = "@odata.nextLink")]
+        public string NextLink { get; set; }
+
+        [DataMember(Name = "@odata.count")]
+        public long? Count { get; set; }
+
+        [DataMember(Name = "value")]
+        public IReadOnlyList<IReadOnlyDictionary<string, string>> Value { get; set; }
+
+        [IgnoreDataMember]
+        public EdgeApiErrorValue ErrorValue { get; set; }
+    }
+}
